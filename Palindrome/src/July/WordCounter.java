ea2dc87023f3bc14d7b00ff697f98f9fafa1ab27
Kismat Shrestha <kismatds08@gmail.com>
2020-07-07 09:43:06 -0500

    Initial commit

---
+++ Palindrome/src/July/WordCounter.java
@@ -0,0 +1,20 @@
+package July;
+/*5. Write a Java method to count all words in a string. Go to the editor
+        Test Data:
+        Input the string: The quick brown fox jumps over the lazy dog.
+        Expected Output:
+
+        Number of words in the string: 9
+ */
+
+import java.util.Scanner;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the string: ");
+        String str = in.nextLine();
+
+
+    }
+}
